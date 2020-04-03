8086dd391ab8020fb66f7feb5fea5ff457eb9646
Sunder20 <sunder20@gmail.com>
2020-04-03 21:38:40 +0530

    Added HTML and CSS - Level 6 Milestone

--- create_users_table.rb
+++
@@ -1,8 +0,0 @@
-require './connect_db.rb'
-connect_db!
-
-ActiveRecord::Migration.create_table(:users) do |t|
-  t.column :name, :text
-  t.column :email, :text
-  t.column :password, :text  
-end
---
+++ db/migrate/20200403124835_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.column :name, :text
+      t.column :email, :text
+      t.column :password, :text
+    end
+  end
+end
