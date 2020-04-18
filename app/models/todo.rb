897ba0511a60ae6fed236908be4e7188e13ec828
Sunder20 <sunder20@gmail.com>
2020-04-19 01:59:05 +0530

    Validate todos before saving

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,9 @@
 class Todo < ActiveRecord::Base
+  validates :todo_text, presence: true
+  validates :todo_text, length: {minimum: 2}
+  validates :due_date, presence: true
+  belongs_to :user
+
   def due_today?
     due_date == Date.today
   end
