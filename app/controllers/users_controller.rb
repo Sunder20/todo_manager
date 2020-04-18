e7e8d115ac6c3a4608f12affd7c10ee3121c356f
Sunder20 <sunder20@gmail.com>
2020-04-19 02:15:50 +0530

    Adding Level 9 Milestone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    new_user = User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-
-    redirect_to root_path
+    if new_user.save
+      flash[:error] = "User Registered. Sign-in to continue"
+      redirect_to root_path
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :email, presence: true
   has_secure_password
   has_many :todos
 end
