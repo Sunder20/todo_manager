54c0935d83248beb6f368814f2fc5acb908c9a4b
Sunder20 <sunder20@gmail.com>
2020-05-02 16:04:55 +0530

    Formatted code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
   has_secure_password
   has_many :todos
 end
